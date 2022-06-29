e69a3e61632f7eb7ea26fb49c3fb273e0517bb46
Patrick Ostie <patrick.ostie@stroeer.de>
2022-06-30 01:40:59 +0200

    fix: readd accidentally deleted local var

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ locals {
   resource_id             = var.resource.new_path != null ? aws_api_gateway_resource.resource[0].id : data.aws_api_gateway_resource.resource[0].id
   resource_path           = var.resource.new_path != null ? aws_api_gateway_resource.resource[0].path : data.aws_api_gateway_resource.resource[0].path
   resource_last_path_part = var.resource.new_path != null ? aws_api_gateway_resource.resource[0].path_part : data.aws_api_gateway_resource.resource[0].path_part
+  has_iam_method_policies = var.authorization.auth_type == "AWS_IAM" && can(length(var.authorization.allow_for) > 0)
   has_authorizer_attached = var.authorization.auth_type == "CUSTOM" && var.authorization.authorizer != null
 }
 
